58e070cc29c8ed36c995737532a1740691ce7a96
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-19 14:04:11 +0800

    教程用的是slot，我改用了v-slot(之前CLI2默认Vue版本低于2.6，不支持slot)，毕竟官方推荐v-slot

--- src/components/content/mainTabbar/MainTabBar.vue
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -2,27 +2,51 @@
   <tab-bar>
 <!--    <tab-bar-item path="/home" activeColor="blue">-->
     <tab-bar-item path="/home">
-      <img slot="item-icon" src="~assets/img/tabbar/home.svg" alt=""/>
-      <img slot="item-icon-active" src="~assets/img/tabbar/home_active.svg" alt=""/>
-      <div slot="item-text">首页</div>
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/home.svg" alt=""/>
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/home_active.svg" alt=""/>
+      </template>
+      <template v-slot:item-text>
+        <div>首页</div>
+      </template>
     </tab-bar-item>
 <!--    <tab-bar-item path="/category" activeColor="green">-->
     <tab-bar-item path="/category">
-      <img slot="item-icon" src="~assets/img/tabbar/category.svg" alt=""/>
-      <img slot="item-icon-active" src="~assets/img/tabbar/category_active.svg" alt=""/>
-      <div slot="item-text">分类</div>
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/category.svg" alt=""/>
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/category_active.svg" alt=""/>
+      </template>
+      <template v-slot:item-text>
+        <div>分类</div>
+      </template>
     </tab-bar-item>
 <!--    <tab-bar-item path="/cart" activeColor="purple">-->
     <tab-bar-item path="/cart">
-      <img slot="item-icon" src="~assets/img/tabbar/shopcart.svg" alt=""/>
-      <img slot="item-icon-active" src="~assets/img/tabbar/shopcart_active.svg" alt=""/>
-      <div slot="item-text">购物车</div>
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/shopcart.svg" alt=""/>
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/shopcart_active.svg" alt=""/>
+      </template>
+      <template v-slot:item-text>
+        <div>购物车</div>
+      </template>
     </tab-bar-item>
 <!--    <tab-bar-item path="/profile" activeColor="yellow">-->
     <tab-bar-item path="/profile">
-      <img slot="item-icon" src="~assets/img/tabbar/profile.svg" alt=""/>
-      <img slot="item-icon-active" src="~assets/img/tabbar/profile_active.svg" alt=""/>
-      <div slot="item-text">我的</div>
+      <template v-slot:item-icon>
+        <img src="~assets/img/tabbar/profile.svg" alt=""/>
+      </template>
+      <template v-slot:item-icon-active>
+        <img src="~assets/img/tabbar/profile_active.svg" alt=""/>
+      </template>
+      <template v-slot:item-text>
+        <div>我的</div>
+      </template>
     </tab-bar-item>
   </tab-bar>
 </template>
