477b09445f61c7bdc36a42c4c61f9d51079ed18d
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-22 16:41:14 +0800

    156首页开发-推荐信息的展示，这集亮点主要是稍微回顾一下css操作吧。因为组件引入和155差不多的

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -3,27 +3,24 @@
   <div id="home"><!--首页是唯一的，所以给个id-->
     <nav-bar class="home-nav"><template v-slot:center>购物街</template></nav-bar>
     <home-swiper :banners="banners"></home-swiper>
+    <recommend-view :recommends="recommends"></recommend-view>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
   import HomeSwiper from './childComps/HomeSwiper'
+  import RecommendView from './childComps/RecommendView'
 
   import {getHomeMutltidata} from "network/home";
-  //如果该组件没有一个index or 封装好的Vue组件，就需要分别导入
-  // import Swiper from 'components/common/swiper/Swiper';
-  // import SwiperItem from 'components/common/swiper/SwiperItem'
-  //会自动去该目录下寻找index.js，所以可以省略/index
-  //这里直接把组件引用再另外抽离出来了。
-  // import {Swiper, SwiperItem} from 'components/common/swiper'
 
 
   export default {
     name: "Home",
     components: {
       NavBar,
-      HomeSwiper
+      HomeSwiper,
+      RecommendView
     },
     data(){
       return {
---
+++ src/views/home/childComps/RecommendView.vue
@@ -0,0 +1,44 @@
+<template xmlns:a="http://www.w3.org/1999/html">
+  <div class="recommend">
+    <div v-for="item in recommends" class="recommend-item">
+      <a :href="item.link">
+        <img :src="item.image">
+        <div>{{item.title}}</div>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "RecommendView",
+    props:{
+      recommends:{
+        type:Array,
+        default(){
+          return []
+        }
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .recommend{
+    display:flex; /*盒子布局*/
+    text-align: center; /*里面内容剧中*/
+    font-size: 12px;
+    padding: 10px 0 20px;/*上下留白， 4个参数分别上右下左*/
+    border-bottom: 10px solid #eee;/* 下面留一个浅色横条，画面过渡比较自然 */
+  }
+
+  .recommend-item{
+    flex: 1; /*内部组件均等分布局（只有里面内容width总和小于父类才生效）*/
+  }
+
+  .recommend-item img{
+    width: 70px;
+    height: 70px;
+    margin-bottom: 10px;
+  }
+</style>
