1d35e84574fc4aec8da84d966442f5ff3fdec01e
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-22 16:18:44 +0800

    155 首页开发-轮播图的展示，没有数据，所以看不到效果。不过跟着教程敲一下，熟悉一下流程。暂时不打算自己实现一个轮播图。先把教程按流程过一遍

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,22 +1,29 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <!--  <h2>首页</h2>-->
   <div id="home"><!--首页是唯一的，所以给个id-->
-    <nav-bar class="home-nav">
-      <template v-slot:center>
-        购物街
-      </template>
-    </nav-bar>
+    <nav-bar class="home-nav"><template v-slot:center>购物街</template></nav-bar>
+    <home-swiper :banners="banners"></home-swiper>
   </div>
 </template>
 
 <script>
   import NavBar from 'components/common/navbar/NavBar'
+  import HomeSwiper from './childComps/HomeSwiper'
+
   import {getHomeMutltidata} from "network/home";
+  //如果该组件没有一个index or 封装好的Vue组件，就需要分别导入
+  // import Swiper from 'components/common/swiper/Swiper';
+  // import SwiperItem from 'components/common/swiper/SwiperItem'
+  //会自动去该目录下寻找index.js，所以可以省略/index
+  //这里直接把组件引用再另外抽离出来了。
+  // import {Swiper, SwiperItem} from 'components/common/swiper'
+
 
   export default {
     name: "Home",
     components: {
       NavBar,
+      HomeSwiper
     },
     data(){
       return {
