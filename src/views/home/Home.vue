50dbca329ee38032b4dc1dbdd85a7d8e32eb5946
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-23 00:17:50 +0800

    161首页开发-首页商品数据的展示,伪类before,after; display:flex;flex-wrap:wrap;justify-content:space-around;

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -8,6 +8,7 @@
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
     <tab-control :titles="['流行','新款','精选']"></tab-control>
+    <goods-list :goods="goods['pop'].list"></goods-list>
     <ul>
       <li>列表1</li>
       <li>列表2</li>
@@ -120,6 +121,7 @@
 
   import NavBar from 'components/common/navbar/NavBar'
   import TabControl from 'components/content/tabControl/TabControl'
+  import GoodsList from 'components/content/goods/GoodsList'
 
   import {getHomeMutltidata, getHomeGoods,} from "network/home";
 
@@ -131,7 +133,8 @@
       RecommendView,
       FeatureView,
       NavBar,
-      TabControl
+      TabControl,
+      GoodsList
     },
     data() {
       return {
@@ -193,5 +196,6 @@
   .tab-control {
     position: sticky;
     top: 44px;
+    z-index: 9;
   }
 </style>
