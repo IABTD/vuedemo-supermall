0d4c4c6ddcde1285670a2937f86e873af77fb837
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-23 17:36:37 +0800

    166 首页开发-BScroll的封装以及使用 ,学会计算显示的高度height

---
+++ src/components/common/scroll/Scroll.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="wrapper" ref="wrapper">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+
+  export default {
+    name: "Scroll",
+    data(){
+      return{
+        scroll:null,
+      }
+    },
+    mounted() {
+      //不要通过query去拿，class重复之类的，可能出现问题
+      // this.scroll = new BScroll(document.querySelector('wrapper'),{
+      this.scroll = new BScroll(this.$refs.wrapper,{//用ref保证一定拿到的是上面指定的ref元素
+        probeType:3
+      })
+      this.scroll.on('scroll',(position)=>{
+        console.log(position);
+      })
+    }
+
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,122 +1,126 @@
 <template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
   <!--  <h2>首页</h2>-->
-  <div id="home"><!--首页是唯一的，所以给个id-->
+  <div id="home" class="wrapper"><!--首页是唯一的，所以给个id-->
     <nav-bar class="home-nav">
       <template v-slot:center>购物街</template>
     </nav-bar>
-    <div class="wrapper">
+
+    <scroll class="content">
+      <home-swiper :banners="banners"></home-swiper>
+      <recommend-view :recommends="recommends"></recommend-view>
+      <feature-view></feature-view>
+      <tab-control class="tab-control"
+                   :titles="['流行','新款','精选']"
+                   @tabClick="tabClick"></tab-control>
+      <goods-list :goods="showGoods"></goods-list>
+      <ul>
+        <li>首页列表1</li>
+        <li>首页列表2</li>
+        <li>首页列表3</li>
+        <li>首页列表4</li>
+        <li>首页列表5</li>
+        <li>首页列表6</li>
+        <li>首页列表7</li>
+        <li>首页列表8</li>
+        <li>首页列表9</li>
+        <li>首页列表10</li>
+        <li>首页列表11</li>
+        <li>首页列表12</li>
+        <li>首页列表13</li>
+        <li>首页列表14</li>
+        <li>首页列表15</li>
+        <li>首页列表16</li>
+        <li>首页列表17</li>
+        <li>首页列表18</li>
+        <li>首页列表19</li>
+        <li>首页列表20</li>
+        <li>首页列表21</li>
+        <li>首页列表22</li>
+        <li>首页列表23</li>
+        <li>首页列表24</li>
+        <li>首页列表25</li>
+        <li>首页列表26</li>
+        <li>首页列表27</li>
+        <li>首页列表28</li>
+        <li>首页列表29</li>
+        <li>首页列表30</li>
+        <li>首页列表31</li>
+        <li>首页列表32</li>
+        <li>首页列表33</li>
+        <li>首页列表34</li>
+        <li>首页列表35</li>
+        <li>首页列表36</li>
+        <li>首页列表37</li>
+        <li>首页列表38</li>
+        <li>首页列表39</li>
+        <li>首页列表40</li>
+        <li>首页列表41</li>
+        <li>首页列表42</li>
+        <li>首页列表43</li>
+        <li>首页列表44</li>
+        <li>首页列表45</li>
+        <li>首页列表46</li>
+        <li>首页列表47</li>
+        <li>首页列表48</li>
+        <li>首页列表49</li>
+        <li>首页列表50</li>
+        <li>首页列表51</li>
+        <li>首页列表52</li>
+        <li>首页列表53</li>
+        <li>首页列表54</li>
+        <li>首页列表55</li>
+        <li>首页列表56</li>
+        <li>首页列表57</li>
+        <li>首页列表58</li>
+        <li>首页列表59</li>
+        <li>首页列表60</li>
+        <li>首页列表61</li>
+        <li>首页列表62</li>
+        <li>首页列表63</li>
+        <li>首页列表64</li>
+        <li>首页列表65</li>
+        <li>首页列表66</li>
+        <li>首页列表67</li>
+        <li>首页列表68</li>
+        <li>首页列表69</li>
+        <li>首页列表70</li>
+        <li>首页列表71</li>
+        <li>首页列表72</li>
+        <li>首页列表73</li>
+        <li>首页列表74</li>
+        <li>首页列表75</li>
+        <li>首页列表76</li>
+        <li>首页列表77</li>
+        <li>首页列表78</li>
+        <li>首页列表79</li>
+        <li>首页列表80</li>
+        <li>首页列表81</li>
+        <li>首页列表82</li>
+        <li>首页列表83</li>
+        <li>首页列表84</li>
+        <li>首页列表85</li>
+        <li>首页列表86</li>
+        <li>首页列表87</li>
+        <li>首页列表88</li>
+        <li>首页列表89</li>
+        <li>首页列表90</li>
+        <li>首页列表91</li>
+        <li>首页列表92</li>
+        <li>首页列表93</li>
+        <li>首页列表94</li>
+        <li>首页列表95</li>
+        <li>首页列表96</li>
+        <li>首页列表97</li>
+        <li>首页列表98</li>
+        <li>首页列表99</li>
+        <li>首页列表100</li>
+      </ul>
+    </scroll>
+
+    <!--<div class="wrapper">
       <div class="content">
-        <home-swiper :banners="banners"></home-swiper>
-        <recommend-view :recommends="recommends"></recommend-view>
-        <feature-view></feature-view>
-        <tab-control class="tab-control"
-                     :titles="['流行','新款','精选']"
-                     @tabClick="tabClick"></tab-control>
-        <goods-list :goods="showGoods"></goods-list>
-        <ul>
-          <li>首页列表1</li>
-          <li>首页列表2</li>
-          <li>首页列表3</li>
-          <li>首页列表4</li>
-          <li>首页列表5</li>
-          <li>首页列表6</li>
-          <li>首页列表7</li>
-          <li>首页列表8</li>
-          <li>首页列表9</li>
-          <li>首页列表10</li>
-          <li>首页列表11</li>
-          <li>首页列表12</li>
-          <li>首页列表13</li>
-          <li>首页列表14</li>
-          <li>首页列表15</li>
-          <li>首页列表16</li>
-          <li>首页列表17</li>
-          <li>首页列表18</li>
-          <li>首页列表19</li>
-          <li>首页列表20</li>
-          <li>首页列表21</li>
-          <li>首页列表22</li>
-          <li>首页列表23</li>
-          <li>首页列表24</li>
-          <li>首页列表25</li>
-          <li>首页列表26</li>
-          <li>首页列表27</li>
-          <li>首页列表28</li>
-          <li>首页列表29</li>
-          <li>首页列表30</li>
-          <li>首页列表31</li>
-          <li>首页列表32</li>
-          <li>首页列表33</li>
-          <li>首页列表34</li>
-          <li>首页列表35</li>
-          <li>首页列表36</li>
-          <li>首页列表37</li>
-          <li>首页列表38</li>
-          <li>首页列表39</li>
-          <li>首页列表40</li>
-          <li>首页列表41</li>
-          <li>首页列表42</li>
-          <li>首页列表43</li>
-          <li>首页列表44</li>
-          <li>首页列表45</li>
-          <li>首页列表46</li>
-          <li>首页列表47</li>
-          <li>首页列表48</li>
-          <li>首页列表49</li>
-          <li>首页列表50</li>
-          <li>首页列表51</li>
-          <li>首页列表52</li>
-          <li>首页列表53</li>
-          <li>首页列表54</li>
-          <li>首页列表55</li>
-          <li>首页列表56</li>
-          <li>首页列表57</li>
-          <li>首页列表58</li>
-          <li>首页列表59</li>
-          <li>首页列表60</li>
-          <li>首页列表61</li>
-          <li>首页列表62</li>
-          <li>首页列表63</li>
-          <li>首页列表64</li>
-          <li>首页列表65</li>
-          <li>首页列表66</li>
-          <li>首页列表67</li>
-          <li>首页列表68</li>
-          <li>首页列表69</li>
-          <li>首页列表70</li>
-          <li>首页列表71</li>
-          <li>首页列表72</li>
-          <li>首页列表73</li>
-          <li>首页列表74</li>
-          <li>首页列表75</li>
-          <li>首页列表76</li>
-          <li>首页列表77</li>
-          <li>首页列表78</li>
-          <li>首页列表79</li>
-          <li>首页列表80</li>
-          <li>首页列表81</li>
-          <li>首页列表82</li>
-          <li>首页列表83</li>
-          <li>首页列表84</li>
-          <li>首页列表85</li>
-          <li>首页列表86</li>
-          <li>首页列表87</li>
-          <li>首页列表88</li>
-          <li>首页列表89</li>
-          <li>首页列表90</li>
-          <li>首页列表91</li>
-          <li>首页列表92</li>
-          <li>首页列表93</li>
-          <li>首页列表94</li>
-          <li>首页列表95</li>
-          <li>首页列表96</li>
-          <li>首页列表97</li>
-          <li>首页列表98</li>
-          <li>首页列表99</li>
-          <li>首页列表100</li>
-        </ul>
       </div>
-    </div>
+    </div>-->
   </div>
 </template>
 
@@ -128,6 +132,7 @@
   import NavBar from 'components/common/navbar/NavBar'
   import TabControl from 'components/content/tabControl/TabControl'
   import GoodsList from 'components/content/goods/GoodsList'
+  import Scroll from 'components/common/scroll/Scroll'
 
   import {getHomeMutltidata, getHomeGoods,} from "network/home";
 
@@ -140,7 +145,8 @@
       FeatureView,
       NavBar,
       TabControl,
-      GoodsList
+      GoodsList,
+      Scroll
     },
     data() {
       return {
@@ -213,7 +219,9 @@
 <style scoped>
 
   #home {
-    padding-top: 44px;
+    /*padding-top: 44px;*/
+    height: 100vh;
+    position: relative;
   }
 
   .home-nav {
@@ -232,4 +240,18 @@
     top: 44px;
     z-index: 9;
   }
+
+  .content{
+    overflow: hidden;
+    position:absolute;
+    top:44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+  /*.content{*/
+  /*  height: calc(100% - 93px);*/
+  /*  overflow: hidden;*/
+  /*  margin-top:44px;*/
+  /*}*/
 </style>
