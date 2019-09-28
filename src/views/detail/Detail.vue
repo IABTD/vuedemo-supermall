ad33bff5c7b5a69c44be3065c667aba57aa337d7
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-28 19:52:34 +0800

    184 详情页-商品详情数据展示，又是因为没有图源的关系，所以显示不正常。。。没有信息的情况下也调不了啥。就学个思路了。回顾watch，监听属性变化

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -1,8 +1,12 @@
 <template>
   <div id="detail">
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :top-images="topImages"></detail-swiper>
-    <detail-base-info :goods="goods"></detail-base-info>
+    <detail-nav-bar class="detail-nav"></detail-nav-bar>
+    <scroll class="content" ref="scroll">
+      <detail-swiper :top-images="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detail-info="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+    </scroll>
   </div>
 </template>
 
@@ -10,8 +14,12 @@
   import DetailNavBar from './childComps/DetailNavBar'
   import DetailSwiper from './childComps/DetailSwiper'
   import DetailBaseInfo from './childComps/DetailBaseInfo'
+  import DetailShopInfo from './childComps/DetailShopInfo'
+  import DetailGoodsInfo from './childComps/DetailGoodsInfo'
 
-  import {getDetail,Goods} from "@/network/detail";
+  import Scroll from 'components/common/scroll/Scroll'
+
+  import {getDetail,Goods,Shop} from "@/network/detail";
 
   export default {
     name: "Detail",
@@ -19,13 +27,18 @@
       return {
         iid:null,
         topImages:[],
-        goods:null
+        goods:{},
+        shop:{},
+        detailInfo:{}
       }
     },
     components:{
       DetailNavBar,
       DetailSwiper,
-      DetailBaseInfo
+      DetailBaseInfo,
+      DetailShopInfo,
+      DetailGoodsInfo,
+      Scroll,
     },
     created() {
       // 1.保存传入的iid
@@ -39,11 +52,37 @@
 
         // 2.获取商品信息
         this.goods = new Goods(data.itemInfo,data.columns,data.showInfo.services)
+
+        // 3.创建店铺信息的对象
+        this.shop = new Shop(data.shopInfo)
+
+        // 4.保存商品的详细数据
+        this.detailInfo = data.detailInfo
       })
+    },
+    methods:{
+      imageLoad(){
+        this.$refs.scroll.refresh()
+      }
     }
   }
 </script>
 
 <style scoped>
+  #detail{
+    position:relative;
+    z-index:9;
+    background-color: #fff;
+    height: 100vh;
+  }
 
+  .detail-nav{
+    position:relative;
+    z-index:9;
+    background-color: #fff;
+  }
+
+  .content{
+    height: calc(100% - 44px);/*减去底部*/
+  }
 </style>
