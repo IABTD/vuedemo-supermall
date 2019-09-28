c458e1e293829e74f18fd444ee4de979d5a9b968
Ashiamd <32120129+Ashiamd@users.noreply.github.com>
2019-09-28 15:22:48 +0800

    180 详情页-数据请求以及轮播图展示, 记得<keep-alive>的exclude指定的vue需要组件Vue有name属性

--- src/components/content/goods/GoodsListItem.vue
+++ src/components/content/goods/GoodsListItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="goods-item">
+  <div class="goods-item" @click="itemClick">
     <img src="goodsItem.show.img" @load="imageLoad"/>
     <div class="goods-info">
       <p>{{goodsItem.title}}</p>
@@ -24,6 +24,16 @@
       imageLoad(){
         // console.log('imageLoad');
         this.$bus.$emit('itemImageLoad')
+      },
+      itemClick(){
+        //跳转到对应的详情页面,这个是动态路由的方式
+        this.$router.push('/detail/'+this.goodsItem.iid)
+        // this.$router.push({
+        //   path:'/detail',
+        //   query:{
+        //     //query方式
+        //   }
+        // })
       }
     }
   }
---
+++ src/views/detail/childComps/DetailNavBar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <nav-bar>
+      <template v-slot:left>
+        <div class="back" @click="backClick">
+          <img src="~assets/img/common/back.svg" alt="">
+        </div>
+      </template>
+      <template v-slot:center >
+        <div class="title">
+          <div v-for="(item,index) in titles"
+               class="title-item"
+               :class="{activate:currentIndex===index}"
+               @click="titleClick(index)">{{item}}</div>
+        </div>
+      </template>
+    </nav-bar>
+  </div>
+</template>
+
+<script>
+  import NavBar from "@/components/common/navbar/NavBar";
+
+  export default {
+    name: "DetailNavBar",
+    components: {
+      NavBar
+    },
+    data(){
+      return{
+        titles:['商品','参数','评论','推荐'],
+        currentIndex:0,
+      }
+    },
+    methods:{
+      titleClick(index){
+        this.currentIndex = index;
+      },
+      backClick(){
+        // this.$router.go(-1)
+        this.$router.back()
+        // this.$router.
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .title{
+    display:flex;
+    font-size: 13px;;
+  }
+  .title-item{
+    flex:1;
+  }
+  .activate{
+    color: var(--color-high-text)
+  }
+  .back{
+    margin-top: 8px;
+  }
+</style>
